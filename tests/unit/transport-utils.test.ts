232ac8f2a09605c94d71d4bd1760745184843b98
Quack6765 <5446230+Quack6765@users.noreply.github.com>
2026-05-20 10:38:59 -0400

    chore: remove unused normalizeMarkdownLineBreaks

--- tests/unit/transport-utils.test.ts
+++ tests/unit/transport-utils.test.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 import { badRequest, ok } from "@/lib/http";
 import { encodeSseEvent, encodeSseFlushMarker, encodeSsePrelude } from "@/lib/sse";
-import { formatTimestamp, normalizeLineBreaks, normalizeMarkdownLineBreaks } from "@/lib/text-utils";
+import { formatTimestamp, normalizeLineBreaks } from "@/lib/text-utils";
 import { cn } from "@/lib/utils";
 
 describe("transport helpers", () => {
@@ -30,6 +30,5 @@ describe("transport helpers", () => {
     expect(formatTimestamp("2026-03-26T15:20:00.000Z")).toMatch(/Mar/);
     expect(normalizeLineBreaks("One\\nTwo")).toBe("One\nTwo");
     expect(normalizeLineBreaks("One\\\\nTwo")).toBe("One\nTwo");
-    expect(normalizeMarkdownLineBreaks("One\\\\n\\\\n\\\\nTwo")).toBe("One\n\n\u00A0\n\nTwo");
   });
 });
